f3177da208841b4feb97594c2f81678132f38b9e
azbin-spec <dydouhabdllah82@gmail.com>
2024-10-22 06:46:40 +0000

    5-printf.c

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,22 +1,14 @@
 #include <stdio.h>
 
 /**
-  * main - Prints the sizeof many var types
-  *
-  * Return: Always 0 (Success)
-  */
+ * main - Prints "with proper grammar, but the outcome is
+ *                a piece of art," followed by a new line.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
-        char a;
-        int b;
-        long int c;
-        long long int d;
-        float e;
+        printf("with proper grammar, but the outcome is a piece of art,\n");
 
-        printf("Size of a char: %d byte(s)\n", sizeof(a));
-        printf("Size of an int: %d byte(s)\n", sizeof(b));
-        printf("Size of a long int: %d byte(s)\n", sizeof(c));
-        printf("Size of a long long int: %d byte(s)\n", sizeof(d));
-        printf("Size of a float: %d byte(s)\n", sizeof(e));
         return (0);
 }
