ed5b1e4290f7a4c114a499b2975c09d8a193b715
azbin-spec <dydouhabdellah82@gmail.com>
2024-10-23 06:49:03 +0000

    0-positive_or_negative.c

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,1 +1,32 @@
-gcc -o positive_or_negative -Wall -Werror -Wextra -pedantic -std=gnu89 0-positive_or_negative.c 
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - Prints if number is positive, zero or negative
+ *
+ * Return: Always (Success)
+ */
+
+int main(void)
+{
+        int n;
+
+        srand(time(0));
+        n = rand() - RAND_MAX / 2;
+
+        if (n > 0)
+        {
+                printf("%d is positive\n", n);
+        }
+        else if (n == 0)
+        {
+                printf("%d is zero\n", n);
+        }
+        else
+        {
+                printf("%d is negative\n", n);
+        }
+
+        return (0);
+} 
