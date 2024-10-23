eb66320193a67e10d4a53068c464131dc1724761
azbin-spec <dydouhabdellah82@gmail.com>
2024-10-23 09:12:50 +0000

    4-print_alphabt.c

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -1,23 +1,23 @@
 #include <stdio.h>
 
 /**
-  * main - Prints some letters of the alphabet
+  * main - Prints some letters of alphabet
   *
-  * Return: Always 0 (Success)
+  * Return: Always (Success)
   */
 int main(void)
 {
-    char c;
+	char c;
 
-    for (c = 'a'; c <= 'z'; c++)
-    {
-        if (c != 'e' && c != 'q')
-        {
-            putchar(c);
-        }
-    }
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		if (c != 'e' && c != 'q')
+		{
+			putchar(c);
+		}
+	}
 
-    putchar('\n');
+	putchar('\n');
 
-    return (0);
+	return (0);
 }		
