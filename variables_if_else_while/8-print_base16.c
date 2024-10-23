35298372c112263ef65134f838578d49df760828
azbin-spec <dydouhabdellah82@gmail.com>
2024-10-23 12:49:26 +0000

    8-print_base16.c

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -1,20 +1,25 @@
 #include <stdio.h>
 
 /**
-  * main - Prints the numbers since 0 to 9
+  * main - Prints a hexadecimal string
   *
   * Return: Always (Success)
   */
 int main(void)
 {
-        char n;
+	char c;
 
-        for (n = '0'; n <= '16'; n++)
-        {
-                putchar(n);
-        }
+	for (c = '0'; c <= '9'; c++)
+	{
+		putchar(c);
+	}
 
-        putchar('\n');
+	for (c = 'a'; c <= 'f'; c++)
+	{
+		putchar(c);
+	}
 
-        return (0);
+	putchar('\n');
+
+	return (0);
 }
