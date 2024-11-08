b106f67bbe509864c247175e41a95d5f9f2d4c08
azbin-spec <dydouhabdellah82@gmail.com>
2024-11-08 19:03:16 +0000

    0-puts_recursion.c

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -1,10 +1,29 @@
-#include <stdio.h>
+#include "main.h"
+/**
+ * square - mdm
+ * @i: i the number
+ * @n: n from the sqrt
+ * Return: Cooucou
+ */
+int square(int n, int i)
+{
+	if ((i * i) > n)
+		return (-1);
 
-void _puts_recursion(char *s) {
-    if (*s) {
-        putchar(*s);
-        _puts_recursion(s + 1);
-    } else {
-        putchar('\n');
-    }
+	if (i * i == n)
+		return (i);
+return (square(n, i + 1));
+}
+
+
+
+/**
+ * _sqrt_recursion - square number of a num
+ * @n: number in question
+ * Return: the square
+ */
+int _sqrt_recursion(int n)
+{
+	n = square(n, 1);
+return (n);
 }
