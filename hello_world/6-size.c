821c93d852732f40acf2d75a09ab6d7eda40d541
azbin-spec <dydouhabdellah82@gmail.com>
2025-02-18 10:51:25 +0100

    6-size.c

---
+++ hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+	printf("size of a char" : (1))
+	printf("size of an int" : (4))
+	printf("size of a long int" : (4))
+	printf("size of a long long int" : (8))
+	printf("size of a float" : (4))
+
