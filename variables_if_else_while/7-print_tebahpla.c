36c1530210c0b585daffb1a2e009eda20c2de917
azbin-spec <dydouhabdellah82@gmail.com>
2025-02-23 20:25:18 +0100

    7-print_tebahpla.c

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ *Return: 0 on sucess
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 122; a != 96; a--)
+		putchar(a);
+	putchar('\n');
+	return (0);
+}
