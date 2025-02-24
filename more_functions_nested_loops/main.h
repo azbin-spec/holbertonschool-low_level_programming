67105503065fc17dcbe8e7ed971dbdd5a4738f0a
azbin-spec <dydouhabdellah82@gmail.com>
2025-02-24 09:09:41 +0100

    main.h

---
+++ more_functions_nested_loops/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+ * File: main.h
+ * Desc: Header file containing prototypes for all functions
+ *       used in the more_functions_nested_loops directory.
+ */
+
+int _putchar(char c);
