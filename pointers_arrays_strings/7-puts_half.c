8a8d3c0f8d6543ed56f4844cfe9f0c1cba7ce1e7
azbin-spec <dydouhabdellah82@gmail.com>
2025-03-02 20:52:39 +0100

    .

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * puts_half - print half of strings
+ * if odd arround int
+ * @str: character array
+ */
+
+void puts_half(char *str)
+{
+	int i, j;
+
+	for (i = 0; str[i]; i++)
+		;
+	if (i % 2 == 0)
+	{
+		for (j = i / 2; str[j]; j++)
+			_putchar(str[j]);
+	}
+	else
+	{
+		for (j = (i + 1) / 2; str[j]; j++)
+			_putchar(str[j]);
+	}
+	_putchar('\n');
+}
