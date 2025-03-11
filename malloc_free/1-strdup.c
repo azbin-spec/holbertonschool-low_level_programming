b8274226e8a40c90ed17967c25977745b2517d41
azbin-spec <dydouhabdellah82@gmail.com>
2025-03-11 14:15:26 +0100

    1-strdup.c

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * _strdup - change pointer
+ * @str :copy this string
+ * Return: Always 1 or NULL if == 0
+ */
+
+char *_strdup(char *str)
+{
+	int i = 0;
+	int len = 0;
+	char *dest;
+
+	if (str == NULL)
+		return (NULL);
+
+	while (str[len] != '\0')
+	{
+
+		len++;
+
+	}
+
+	dest = malloc(sizeof(char) * len + 1);
+
+	if (dest == 0)
+		return (NULL);
+
+	for (; i < len; i++)
+		dest[i] = str[i];
+return (dest);
+}
