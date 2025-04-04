819e06e094f328cc77a034412777883dc21a8b63
azbin-spec <dydouhabdellah82@gmail.com>
2025-04-04 13:57:46 +0200

    added the list.h

---
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - function that returns the nth node
+ * of a dlistint_t linked list
+ * @head: dlistint_t list
+ * @index: index of node;
+ *
+ * Return: return n of the node or NULL if the node does not exist.
+ */
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	dlistint_t *current = head;
+	unsigned int i = 0;
+
+	while (i < index)
+	{
+		if (!current)
+			return (NULL);
+		current = current->next;
+		i++;
+	}
+
+	return (current);
+}
---
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,43 @@
+#include "lists.h"
+
+/**
+ * insert_dnodeint_at_index - inserts a new node at a given position
+ * @h: dlistint_t list
+ * @idx: index of the list where the new node should be added
+ * @n: the integer to be added
+ *
+ * Return: the address of the new node, or NULL if it failed
+ */
+
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *new_node;
+	dlistint_t *prev_node;
+	unsigned int i = 0;
+
+	if (idx == 0)
+		return (add_dnodeint(h, n));
+
+	prev_node = *h;
+	while (i < idx - 1)
+	{
+		if (!prev_node)
+			return (NULL);
+		prev_node = prev_node->next;
+		i++;
+	}
+
+	if (!prev_node->next)
+		return (add_dnodeint_end(h, n));
+
+	new_node = malloc(sizeof(dlistint_t));
+	if (!new_node)
+		return (NULL);
+
+	new_node->n = n;
+	new_node->next = prev_node->next;
+	new_node->prev = prev_node;
+	prev_node->next->prev = new_node;
+	prev_node->next = new_node;
+	return (new_node);
+}
